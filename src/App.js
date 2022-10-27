73bbd925441790ca72729c068ad2ad69fdd1025e
Yacouri <zouhiryac1@gmail.com>
2022-10-27 18:15:02 +0100

    feat: add google analytics

--- src/App.js
+++ src/App.js
@@ -7,9 +7,20 @@ import Testimonials from 'components/Testimonials';
 import Thumb from 'components/Thumb';
 import WorkProcess from 'components/WorkProcess';
 import MainLayout from 'layouts';
-import React from 'react';
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga';
 
 const App = () => {
+  useEffect(() => {
+    ReactGA.initialize(process.env.REACT_APP_GA_CODE);
+    const trackPage = (page) => {
+      ReactGA.set({ page });
+      ReactGA.pageview(page);
+    };
+    const page = window.location.pathname + window.location.search;
+    trackPage(page);
+  }, []);
+
   return (
     <MainLayout>
       <Hero />
